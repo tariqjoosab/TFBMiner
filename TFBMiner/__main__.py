1ac1ac3b6672c690bc3189ec5bb4764127d4eb89
Ruth Stoney <ruth.stoney@manchester.ac.uk>
2023-02-21 17:26:08 +0000

    add Instructions.txt

--- TFBMiner/__main__.py
+++ TFBMiner/__main__.py
@@ -49,6 +49,8 @@ def main():
         genome_files = glob.glob(genome_files_path+"\*")
         if len(genome_files)==0:
             genome_files = glob.glob(genome_files_path+"*")
+        if len(genome_files)==0:
+            genome_files = glob.glob(genome_files_path+"/*")
         genome_assemblies_path = get_path("genome_assemblies.csv")
     try:
         genome_assemblies = pd.read_csv(genome_assemblies_path)
